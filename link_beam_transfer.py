00775eb0047b4f906d0358ddd73a50451f619169
hh <hh4142019@outlook.com>
2021-05-17 18:28:52 +0800

    finished

--- link_beam_transfer.py
+++ link_beam_transfer.py
@@ -10,65 +10,60 @@ To transfer frame link beams to shell link beams
 import comtypes.client
 import numpy as np
 
-SapModel = comtypes.client.GetActiveObject(
-    "CSI.ETABS.API.ETABSObject").SapModel
+SapModel = comtypes.client.CreateObject('ETABSv1.Helper').GetObject("CSI.ETABS.API.ETABSObject") .SapModel
 
+#to obtain all frame lbs
+print("读取所有需要转换的连梁")
+core_group = "core"
+SapModel.SelectObj.ClearSelection()
+SapModel.SelectObj.Group(core_group)
+ret = SapModel.SelectObj.GetSelected()
+lbs = [j for i, j in zip(ret[1], ret[2]) if i == 2]
+SapModel.SelectObj.ClearSelection()
 
-transfer_all = True  # I mean it
-ratio_limit = 4  # Shell beams w l/h ratio smaller than this won't be changed
+#to obtain LBs end points and sections
+print("读取连梁端点")
+points = []
+prpt = []
+for i in lbs:
+    prpt.append(SapModel.FrameObj.GetSection(i)[0])
+    points.append(SapModel.FrameObj.GetPoints(i)[:-1])
 
+# to botain LBs h, b, and concretes
+print("读取连梁截面尺寸和混凝土")
+beam_heights = []
+beam_concs = []
+beam_widths = []
+for i in prpt:
+    ret = SapModel.PropFrame.GetRectangle(i)
+    beam_heights.append(ret[2])
+    beam_widths.append(ret[3])
+    beam_concs.append(ret[1])
 
-def euclidean(x, y):
-    return np.sqrt(np.sum((x - y)**2))
+# to create new wall properties for LBs
+print("定义新的墙截面")
+new_wall_prpts = []
+for i, j, k in zip(beam_concs, beam_widths, lbs):
+    new_name = k+"_"+i+"_"+str(j)
+    SapModel.PropArea.SetWall(new_name, 1, 1, i, j)
+    new_wall_prpts.append(new_name)
 
+# to draw new wall like lbs
+print("绘制shell连梁")
+for i, j, k in zip(points, beam_heights, new_wall_prpts):
+    x1, y1, z1 = SapModel.PointObj.GetCoordCartesian(i[0])[:-1]
+    x2, y2, z2 = SapModel.PointObj.GetCoordCartesian(i[1])[:-1]
+    X = [x1, x2, x2, x1]
+    Y = [y1, y2, y2, y1]
+    Z = [z1, z2, z2-j, z1-j]
+    SapModel.AreaObj.AddByCoord(4, X, Y, Z, k, k, k)
 
-story_levels = SapModel.Story.GetStories_2()[3]
-stories = SapModel.Story.GetStories_2()[2]
-count = SapModel.PropArea.Count()
+# to delete original lbs
+print("删除原连梁")
+SapModel.SelectObj.ClearSelection()
+SapModel.FrameObj.SetSelected(core_group, True, 1)
+SapModel.FrameObj.Delete("all", 2)
 
-
-for i, j in zip(stories, story_levels):
-    areas_on_story = SapModel.AreaObj.GetNameListOnStory(i)[1]
-    points_on_story = set(SapModel.PointObj.GetNameListOnStory(i)[1])
-    lbs = []
-    count = []
-    for area in areas_on_story[:]:
-        if SapModel.AreaObj.GetDesignOrientation(area)[0] == 1:
-           # area_prpt =
-            area_points = list(SapModel.AreaObj.GetPoints(area)[1])
-
-            if all([i in points_on_story for i in area_points]):
-                coords = sorted([np.array(SapModel.PointObj.GetCoordCartesian(
-                                k)[:-1]) for k in area_points], key=lambda x: x[-1], reverse=True)[:-1]
-                lb_length = euclidean(coords[0][:-1], coords[1][:-1])
-                lb_height = abs(coords[0][-1]-coords[2][-1])
-                lbs.append(area)
-
-                lb_prpt = SapModel.PropArea.GetWall(
-                    SapModel.AreaObj.GetProperty(area)[0])
-                lb_conc = lb_prpt[2]
-                lb_thickness = lb_prpt[3]
-
-                new_name = "LB"+str(int(lb_thickness))+"x" + \
-                    str(int(lb_height))+"x"+lb_conc
-                SapModel.PropFrame.SetRectangle(
-                    new_name, lb_conc, lb_height, lb_thickness)
-
-                if transfer_all:
-                    print("All link beams are been transfered, don't regret")
-                    count.append(area)
-                    SapModel.AreaObj.Delete(area)
-                    SapModel.FrameObj.AddByCoord(
-                        *coords[0], *coords[1], new_name, new_name)
-
-                elif lb_length/lb_height >= ratio_limit:
-                    count.append(area)
-                    SapModel.AreaObj.Delete(area)
-                    SapModel.FrameObj.AddByCoord(
-                        *coords[0], *coords[1], new_name, new_name)
-
-    print("{} of {} shell LBs have been transfered on {}.".format(
-        len(count), len(lbs), i))
-
-
+SapModel.SelectObj.ClearSelection()
 SapModel.View.RefreshView()
+
